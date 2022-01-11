86919040aa7b0203d1a0ec3ddaceeba27d006cee
Yaniv Holder <yaniv.holder@outlook.com>
2022-01-11 20:48:28 +0200

    Add runClass skeleton

--- src/OOP/Solution/OOPTestSummary.java
+++ src/OOP/Solution/OOPTestSummary.java
@@ -3,6 +3,7 @@ package OOP.Solution;
 import OOP.Provided.OOPResult;
 import java.util.*;
 
+// TODO: test that impl is fine (different from f*)
 public class OOPTestSummary {
 
     private int n_success = 0;
@@ -10,9 +11,9 @@ public class OOPTestSummary {
     private int n_mismatch = 0;
     private int n_error = 0;
 
-    OOPTestSummary (Map<String, OOPResult> testMap){
-        for (OOPResult res : testMap.values()){
-            switch (res.getResultType()){
+    OOPTestSummary (Map<String, OOPResult> testMap) {
+        for (OOPResult res : testMap.values()) {
+            switch (res.getResultType()) {
                 case SUCCESS -> n_success++;
                 case FAILURE -> n_fail++;
                 case EXPECTED_EXCEPTION_MISMATCH -> n_mismatch++;
--- src/OOP/Solution/OOPUnitCore.java
+++ src/OOP/Solution/OOPUnitCore.java
@@ -1,7 +1,18 @@
 package OOP.Solution;
 
 import OOP.Provided.OOPAssertionFailure;
+import OOP.Provided.OOPResult;
+import OOP.Solution.OOPTestClass;
 import OOP.Solution.OOPTestSummary;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class OOPUnitCore {
 
@@ -19,8 +30,47 @@ public class OOPUnitCore {
         return runClass(testClass, null);
     }
 
-    public static OOPTestSummary runClass(Class<?> testClass, String tag) {
+    public static OOPTestSummary runClass(Class<?> testClass, String tag) throws IllegalArgumentException {
+        // TODO: check if the exception should be thrown when tag == null
+        if(testClass == null || testClass.isAnnotationPresent(OOPTestClass.class)) {
+            throw new IllegalArgumentException();
+        }
 
+        // Step 1
+        Object newTestClass = CreateNewTestClass(testClass);
+
+        // Step 2
+        setup(testClass, newTestClass);
+
+        // Steps 3-5
+        Map<String, OOPResult> testMap = runAllTests(testClass, tag, newTestClass);
+
+        // Step 6
+        return new OOPTestSummary(testMap);
+    }
+
+    private static Object CreateNewTestClass(Class<?> testClass) {
+        // TODO: implement
+        return new Object();
+    }
+
+    private static void setup(Class<?> testClass, Object newTestClass) {
+        // TODO: implement
+    }
+
+    private static Map<String, OOPResult> runAllTests(Class<?> testClass, String tag, Object newTestClass) {
+        // TODO: implement
+        return new HashMap<String, OOPResult>();
+    }
+
+    // Run all OOPBefore all OOPAfter methods relevant to testMethod.
+    // @param isBefore: if true, run OOPBefore methods. Otherwise, run OOPAfter
+    // methods.
+    // @return success state: null if all methods ran succefully, otherwise the
+    // class of the exception that was raised.
+    private static Class<? extends Exception> runWithBackup(Method testMethod, Object instance, boolean isBefore) {
+        
+        return null;
     }
 
 
