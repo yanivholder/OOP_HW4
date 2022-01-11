feafe4ca27b5ed31def5b0e6e169ef279b74710c
eilon299 <eilonkornboim@gmail.com>
2022-01-11 18:52:35 +0200

    OOPSetup and OOPTest

--- src/OOP/Solution/OOPSetup.java
+++ src/OOP/Solution/OOPSetup.java
@@ -1,1 +1,17 @@
 package OOP.Solution;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OOPSetup {
+
+/* we want to make sure that a method marked with this annotation will run:     1. only once
+                                                                                2. before testing
+
+ */
+}
--- src/OOP/Solution/OOPTest.java
+++ src/OOP/Solution/OOPTest.java
@@ -1,1 +1,17 @@
 package OOP.Solution;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface OOPTest {
+
+    int order = 0;
+    String tag = "";
+/*
+
+ */
+}
