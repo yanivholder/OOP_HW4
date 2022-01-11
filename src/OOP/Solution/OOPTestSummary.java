d518adc3ac19fae0f29deb87ad97e87253b7fd8c
eilon299 <eilonkornboim@gmail.com>
2022-01-11 19:51:48 +0200

    OOPTestSummery

--- src/OOP/Solution/OOPTestSummary.java
+++ src/OOP/Solution/OOPTestSummary.java
@@ -5,31 +5,40 @@ import java.util.*;
 
 
 public class OOPTestSummary {
-    Map<OOPTestResult, Integer> map = new HashMap<OOPTestResult, Integer>();
-//    map.put()
-    private int n_success;
-    private int n_fail;
-    private int n_mismatch;
-    private int n_error;
+//    Map<OOPResult.OOPTestResult, Integer> map = new HashMap<OOPResult.OOPTestResult, Integer>();
+//    map.put(OOPResult.OOPTestResult.SUCCESS, 0);
+//    map.put(OOPResult.OOPTestResult.SUCCESS, 0);
+//    map.put(OOPResult.OOPTestResult.SUCCESS, 0);
+//    map.put(OOPResult.OOPTestResult.SUCCESS, 0);
+
+    private int n_success = 0;
+    private int n_fail = 0;
+    private int n_mismatch = 0;
+    private int n_error = 0;
 
     OOPTestSummary (Map<String, OOPResult> testMap){
-        for (OOPTestResult res : testMap.values()){
-            switch (res)
+        for (OOPResult res : testMap.values()){
+            switch (res.getResultType()){
+                case SUCCESS -> n_success++;
+                case FAILURE -> n_fail++;
+                case EXPECTED_EXCEPTION_MISMATCH -> n_mismatch++;
+                case ERROR -> n_error++;
+            }
         }
     }
     int getNumSuccesses(){
-
+        return n_success;
     }
 
     int getNumFailures(){
-
+        return n_fail;
     }
 
     int getNumExceptionMismatches(){
-
+        return n_mismatch;
     }
 
     int getNumErrors(){
-
+        return n_error;
     }
 }
