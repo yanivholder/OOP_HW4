61daf47b1eacb6c4f37bcceb76e22b72099fe226
eilon299 <eilonkornboim@gmail.com>
2022-01-16 12:22:15 +0200

    done

--- src/OOP/Solution/OOPUnitCore.java
+++ src/OOP/Solution/OOPUnitCore.java
@@ -4,8 +4,6 @@ import OOP.Provided.OOPAssertionFailure;
 import OOP.Provided.OOPExceptionMismatchError;
 import OOP.Provided.OOPExpectedException;
 import OOP.Provided.OOPResult;
-import OOP.Solution.OOPTestClass;
-import OOP.Solution.OOPTestSummary;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
@@ -59,9 +57,11 @@ public class OOPUnitCore {
 
     private static Object CreateNewTestClass(Class<?> testClass) {
         try {
-            return Arrays.stream(testClass.getDeclaredConstructors())
+            Constructor<?> mt = Arrays.stream(testClass.getDeclaredConstructors())
                     .filter(ctor -> ctor.getParameterCount() == 0)
-                    .findFirst().get().newInstance();
+                    .findFirst().get();
+            mt.setAccessible(true);
+            return mt.newInstance();
 
         }
         catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
@@ -126,7 +126,7 @@ public class OOPUnitCore {
     }
 
     private static OOPResult testRun(Method m, Object obj) {
-        Class<? extends Exception> e = runWithBackup(m, obj, true);
+        Class<? extends Exception> e = run_and_backup(m, obj, true);
         if(e != null) return new OOPResultImpl(OOPResult.OOPTestResult.ERROR, e.getName());
 
         OOPResult res = null;
@@ -182,53 +182,43 @@ public class OOPUnitCore {
         if(res == null) {
             res = new OOPResultImpl(OOPResult.OOPTestResult.SUCCESS, null);
         }
-        e = runWithBackup(m, obj, false);
+        e = run_and_backup(m, obj, false);
         if(e != null) {
             res = new OOPResultImpl(OOPResult.OOPTestResult.ERROR, e.getName());
         }
         return res;
     }
 
-    // A minimal wrapper class for values, because `Collectors.toMap` doesn't
-    // support null values, so we have to wrap them.
-    private static class Value {
+    private static class Val_wrap {
         public Object x;
-        public Value(Object x) {
+        public Val_wrap(Object x) {
             this.x = x;
         }
     }
 
-    // Run all OOPBefore all OOPAfter methods relevant to testMethod.
-    // @param isBefore: if true, run OOPBefore methods. Otherwise, run OOPAfter
-    // methods.
-    // @return success state: null if all methods ran successfully, otherwise the
-    // class of the exception that was raised.
-    private static Class<? extends Exception> runWithBackup(Method testMethod, Object instance, boolean isBefore) {
+    private static Class<? extends Exception> run_and_backup(Method testMethod, Object obj, boolean isBefore) {
         Stream<Method> method_stream = null;
-        if (isBefore) {
-            method_stream = getMethodsWithAnnotation(instance.getClass(), OOPBefore.class, true)
+        Class<?> cls = isBefore ? OOPBefore.class : OOPAfter.class;
+        if (isBefore){
+            method_stream = getMethodStreamWithGivenAnnotation(obj.getClass(), OOPBefore.class, isBefore)
                     .filter(m -> Arrays.asList(m.getAnnotation(OOPBefore.class).value()).contains(testMethod.getName()));
-        } else {
-            method_stream = getMethodsWithAnnotation(instance.getClass(), OOPAfter.class, false)
+        }else{
+            method_stream = getMethodStreamWithGivenAnnotation(obj.getClass(), OOPAfter.class, isBefore)
                     .filter(m -> Arrays.asList(m.getAnnotation(OOPAfter.class).value()).contains(testMethod.getName()));
         }
+
+
+
         List<Method> methods = method_stream.collect(Collectors.toList());
 
-        // execute all before_methods, with short-circuit if any of them cause
-        // an error (short-circuit + lambdas with side effects => a stream isn't
-        // appropriate here)
-
-        // `Collectors.toMap` doesn't allow null values, but fields can have a
-        // null value. We solve the contradiction by wrapping each value in a
-        // Value object. cloneField never returns null, so we are safe.
-        Map<Field, Value> backup_date = Arrays.stream(instance.getClass().getDeclaredFields())
-                .collect(Collectors.toMap(field -> field, field -> cloneField(instance, field)));
+        Map<Field, Val_wrap> backup_date = Arrays.stream(obj.getClass().getDeclaredFields())
+                .collect(Collectors.toMap(field -> field, field -> clone_orCopyConstr_orSaveVal(obj, field)));
         for (Method m : methods) {
             try {
                 m.setAccessible(true);
-                m.invoke(instance);
+                m.invoke(obj);
             } catch (Exception e) {
-                restore(instance, backup_date);
+                restore_obj(obj, backup_date);
                 return e.getClass();
             }
         }
@@ -236,48 +226,9 @@ public class OOPUnitCore {
         return null;
     }
 
-    // @return a sorted stream of all methods of testClass that are annotated by
-    // the given annotation. The result is sorted by order of inheritance: from
-    // methods declared in testClass to methods declared upward in the
-    // inheritance-chain of testClass. The result includes both public and
-    // non-public methods. The result filters out ancestor methods that were
-    // overridden by a child, keeping only the overriding method.
-    // @param topDown: if topDown is True, then the result is sorted in the
-    // reverse order (i.e., from ancestor to child).
-    private static Stream<Method> getMethodsWithAnnotation(Class<?> testClass,
-                                                           Class<? extends Annotation> annotationClass, boolean topDown) {
-        //Stream.Builder<Method> methods = Stream.builder();
-        List<Class<?>> ancestors = sortedAncestors(testClass);
-        if (topDown) {
-            Collections.reverse(ancestors);
-        }
-        List<Method> methods = new ArrayList<>();
-        ancestors.stream()
-                .flatMap(cls -> Arrays.stream(cls.getDeclaredMethods()))
-                .filter(m -> m.isAnnotationPresent(annotationClass))
-                .forEach(method -> {
-                    if (topDown) {
-                        methods.removeIf(m -> method.getName().equals(m.getName()));
-                        methods.add(method);
-                    } else if (methods.stream().filter(m -> method.getName().equals(m.getName())).findAny().isEmpty()) {
-                        methods.add(method);
-                    }
-                });
-        return methods.stream();
-    }
-
-    // @return a list of all cls ancestors, sorted from cls upwards, excluding
-    // the null at the end.
-    private static List<Class<?>> sortedAncestors(Class<?> cls) {
-        List<Class<?>> ancestors = new ArrayList<>();
-        for (; cls != null; cls = cls.getSuperclass()) {
-            ancestors.add(cls);
-        }
-        return ancestors;
-    }
 
     // @return a clone of the specified field of obj
-    private static Value cloneField(Object obj, Field field) {
+    private static Val_wrap clone_orCopyConstr_orSaveVal(Object obj, Field field) {
         Object value = null;
         try {
             field.setAccessible(true);
@@ -288,19 +239,17 @@ public class OOPUnitCore {
         }
 
         if (value == null) {
-            return new Value(null);
+            return new Val_wrap(null);
         }
 
         Class<?> cls = value.getClass();
         if (Cloneable.class.isInstance(value)) {
             try {
-                // can't use cls.getMethod("clone"), because it ignores
-                // protected methods - while clone can be protected. No need to
-                // worry about clone methods with less visibility than protected
-                // (i.e., private clone), because we already checked that cls
-                // implements Cloneable interface (which enforces a visibility
-                // of at least protected).
-                Method clone = sortedAncestors(cls).stream().map(ancestor -> {
+                List<Class<?>> ancestors = new ArrayList<>();
+                for (; cls != null; cls = cls.getSuperclass()) {
+                    ancestors.add(cls);
+                }
+                Method clone = ancestors.stream().map(ancestor -> {
                             try {
                                 return ancestor.getDeclaredMethod("clone");
                             } catch (NoSuchMethodException e) {
@@ -331,26 +280,21 @@ public class OOPUnitCore {
                 e.printStackTrace();
             }
         }
-        return new Value(value);
+        return new Val_wrap(value);
     }
 
 
-    // set the specified field of an object to the specified value. Assumes
-    // `value` is a legal value for the field.
-    private static void setField(Object obj, Field field, Value value) {
+    private static void restore_field(Object obj, Field field, Val_wrap value) {
         field.setAccessible(true);
         try {
             field.set(obj, value.x);
-        } catch (IllegalArgumentException | IllegalAccessException e) {
-            // should never arrive here
-            // e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
         }
     }
 
-    // Set all the fields of `obj` according to their backup in `backup`
-    private static void restore(Object obj, Map<Field, Value> backup) {
-        backup.entrySet().stream().forEach(entry -> setField(obj, entry.getKey(), entry.getValue()));
+    private static void restore_obj(Object obj, Map<Field, Val_wrap> backup) {
+        backup.entrySet().stream().forEach(entry -> restore_field(obj, entry.getKey(), entry.getValue()));
     }
-
 
 }
